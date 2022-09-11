aca19d65fa507ad0d628fcfc6559d3055b875759
TLRKiliann <philogenie@protonmail.com>
2022-09-11 13:13:50 +0200

    Certification almost done

--- my-portfolio/src/Pages/Certifications.jsx
+++ my-portfolio/src/Pages/Certifications.jsx
@@ -1,5 +1,6 @@
 import fccImg from '../assets/images/fcc_primary_small.png';
 import openImg from '../assets/images/openclass_logo.png';
+import secNumImg from '../assets/images/secnum_logo.png';
 import '../styles/certif.scss';
 
 
@@ -14,171 +15,218 @@ export const Certifications = () => {
 
       <section className="section--certif">
         <article className="art--tag">
-          <h3>Truc 111</h3>
+          <h3>2022</h3>
           <div className="flexible--imgfcc">
-            <p>FreeCodeCamp 2021</p>
+            <p>FreeCodeCamp</p>
             <img src={fccImg} width="50px" height="30px" alt="no img"/>
           </div>
           <div className="div--certifications">
             <ul>
               <li>
-                Web designer machin
+                Front End Development Libraries
+                (React - Redux - Bootstrap - jQuerry - Sass)
+              </li>
+              <li>
+                FreeCodeCamp Youtube Videos
               </li>
             </ul>
           </div>
         </article>
 
         <article className="art--tag">
-          <h3>Truc 111</h3>
+          <h3>2021</h3>
           <div className="flexible--imgfcc">
-            <p>FreeCodeCamp 2021-2022</p>
+            <p>FreeCodeCamp</p>
             <img src={fccImg} width="50px" height="30px" alt="no img"/>
           </div>
           <div className="div--certifications">
             <ul>
               <li className="abs--li">
-                Web Frontend Librairies
+                Responsive Web Design
               </li>
             </ul>
           </div>
         </article>
 
         <article className="art--tag">
-          <h3>Truc 111</h3>
+          <h3>2021</h3>
           <div className="flexible--imgoc">
-            <p>OpenClassRoom 2021-2022</p>
+            <p>OpenClassRoom</p>
             <img src={openImg} width="50px" height="30px" alt="no img"/>
           </div>
 
           <div className="div--certifications">
             <ul>
               <li>
-                Python3 perfectionnement
-              </li>
-              <li>
-                Web reseaz
-              </li>
-              <li>
-                TCP/IP
+                Mettez en place votre environnement front-end.
               </li>
             </ul>
           </div>
         </article>
 
         <article className="art--tag">
-          <h3>Truc 111</h3>
+          <h3>2018-2021</h3>
           <div className="flexible--imgoc">
-            <p>OpenClassRoom 2021-2022</p>
+            <p>OpenClassRoom</p>
             <img src={openImg} width="50px" height="30px" alt="no img"/>
           </div>
 
           <div className="div--certifications">
             <ul>
               <li>
+                Apprenez à coder en Javascript
               </li>
               <li>
-              </li>
-              <li>
+                Apprenez à programmer avec JavaScript 
               </li>
             </ul>
           </div>
         </article>
 
         <article className="art--tag">
-          <h3>Truc 111</h3>
+          <h3>2016-2020</h3>
           <div className="flexible--imgoc">
-            <p>OpenClassRoom 2021-2022</p>
+            <p>OpenClassRoom</p>
             <img src={openImg} width="50px" height="30px" alt="no img"/>
           </div>
 
           <div className="div--certifications">
             <ul>
               <li>
+                Python3
               </li>
               <li>
+                POO en Python3
               </li>
               <li>
+                Tester vos projets avec Python3
+              </li>
+              <li>
+                Perfectionnez-vous en Python3
               </li>
             </ul>
           </div>
         </article>
 
         <article className="art--tag">
-          <h3>Truc 111</h3>
+          <h3>2019</h3>
           <div className="flexible--imgoc">
-            <p>OpenClassRoom 2021-2022</p>
+            <p>OpenClassRoom</p>
             <img src={openImg} width="50px" height="30px" alt="no img"/>
           </div>
 
           <div className="div--certifications">
             <ul>
               <li>
-              </li>
-              <li>
-              </li>
-              <li>
+                Gérez votre code avec Git et GitHub 
               </li>
             </ul>
           </div>
         </article>
 
         <article className="art--tag">
-          <h3>Truc 111</h3>
-          <div className="flexible--imgoc">
-            <p>OpenClassRoom 2021-2022</p>
-            <img src={openImg} width="50px" height="30px" alt="no img"/>
+          <h3>2019</h3>
+          <div className="flexible--imgsec">
+            <p>ANSSI</p>
+            <img src={secNumImg} width="50px" height="30px" alt="no img"/>
           </div>
 
           <div className="div--certifications">
             <ul>
               <li>
-              </li>
-              <li>
-              </li>
-              <li>
+                MOOC SecNumAcadémie 
               </li>
             </ul>
           </div>
         </article>
 
         <article className="art--tag">
-          <h3>Truc 111</h3>
+          <h3>2016-2019</h3>
           <div className="flexible--imgoc">
-            <p>OpenClassRoom 2021-2022</p>
+            <p>OpenClassRoom</p>
             <img src={openImg} width="50px" height="30px" alt="no img"/>
           </div>
 
           <div className="div--certifications">
             <ul>
               <li>
+                Comprendre le web
               </li>
               <li>
+                Fonctionnement des réseaux <br/> TCP/IP
               </li>
               <li>
+                Maîtriser vos applications et réseaux TCP/IP
               </li>
             </ul>
           </div>
         </article>
 
         <article className="art--tag">
-          <h3>Truc 111</h3>
+          <h3>2016-2019</h3>
           <div className="flexible--imgoc">
-            <p>OpenClassRoom 2021-2022</p>
+            <p>OpenClassRoom</p>
             <img src={openImg} width="50px" height="30px" alt="no img"/>
           </div>
 
           <div className="div--certifications">
             <ul>
               <li>
+                PHP - MySQL (HTML5 + CSS3)
               </li>
               <li>
-              </li>
-              <li>
+                Server apache2 with PHP - MVC (blog)
               </li>
             </ul>
           </div>
         </article>
 
+        <article className="art--tag">
+          <h3>2015-2016</h3>
+          <div className="flexible--imgoc">
+            <p>OpenClassRoom</p>
+            <img src={openImg} width="50px" height="30px" alt="no img"/>
+          </div>
+
+          <div className="div--certifications">
+            <ul>
+              <li>
+                Protégez l’ensemble de vos données sur votre ordinateur
+              </li>
+              <li>
+                Apprenez à naviguer en sécurité sur Internet
+              </li>
+              <li>
+                Protégez l’ensemble de vos données sur votre ordinateur 
+              </li>
+            </ul>
+          </div>
+        </article>
+
+        <article className="art--tag">
+          <h3>2015</h3>
+          <div className="flexible--imgoc">
+            <p>OpenClassRoom</p>
+            <img src={openImg} width="50px" height="30px" alt="no img"/>
+          </div>
+
+          <div className="div--certifications">
+            <ul>
+              <li>
+                Programmation en C
+              </li>
+              <li>
+                Linux
+              </li>
+              <li>
+                HTML5
+              </li>
+              <li>
+                CSS3
+              </li>
+            </ul>
+          </div>
+        </article>
 
       </section>
     </div>
